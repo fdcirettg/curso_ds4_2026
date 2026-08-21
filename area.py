0ae9d0b2c75a2741ec3ce9a746ce5331201083ca
fdcirettg <federico.cirett@unison.mx>
2026-08-20 17:48:55 -0700

    area works with defaults

--- area.py
+++ area.py
@@ -10,8 +10,8 @@ def calculate_rectangle_area(length, width):
 def main():
     """Main function to parse command line arguments and calculate the area."""
     parser = argparse.ArgumentParser(description="Calculate the area of a rectangle.")
-    parser.add_argument("-l","length", type=float, help="The length of the rectangle.")
-    parser.add_argument("-w","width", type=float, help="The width of the rectangle.")
+    parser.add_argument("-l", "--length", default=10.0, type=float, help="The length of the rectangle.")
+    parser.add_argument("-w", "--width", default=5.0, type=float, help="The width of the rectangle.")
     args = parser.parse_args()
 
     area = calculate_rectangle_area(args.length, args.width)
