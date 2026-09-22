fdb3eac5f4bd2cd1c38f2af1d053e6076fbdaac9
fdcirettg <federico.cirett@unison.mx>
2026-09-21 17:29:50 -0700

    Added funtctions to create a list of words

--- Hang_man/auxiliary_functions.py
+++ Hang_man/auxiliary_functions.py
@@ -6,6 +6,12 @@ def load_book(file_path:str) -> list:
         words = file.read().split()
     return words
 
+def save_words_to_file(word_list:list, file_path:str) -> None:
+    """Save a list of words to a text file."""
+    with open(file_path, 'w', encoding='utf-8') as file:
+        for word in word_list:
+            file.write(word + '\n')
+
 def clean_list_of_words(word_list:list) -> list:
     """Clean a list of words by removing punctuation and converting to lowercase."""
     cleaned_words = []
@@ -16,6 +22,13 @@ def clean_list_of_words(word_list:list) -> list:
         cleaned_words.append(word)
     return cleaned_words
 
+def reduce_list_of_words(word_list:list, min_length:int=3) -> list:
+    """Reduce a list of words by filtering out words shorter than min_length."""
+    reduced_words = [word for word in word_list if len(word) >= min_length]
+    word_set = set(reduced_words)  # Remove duplicates
+    reduced_words = list(word_set)
+    return reduced_words
+
 if __name__ == "__main__":
     # Example usage
     book_path = "Dracula.txt"
@@ -25,5 +38,9 @@ if __name__ == "__main__":
     #print(words[0])
     #print(words[:200])
     cleaned_words = clean_list_of_words(words)
-    print(cleaned_words)
     print("Number of words in the cleaned list:", len(cleaned_words))
+    reduced_words = reduce_list_of_words(cleaned_words,4)
+    
+    print(reduced_words)
+    print("Number of words in the reduced list:", len(reduced_words))
+    save_words_to_file(reduced_words, "dracula_words.txt")
